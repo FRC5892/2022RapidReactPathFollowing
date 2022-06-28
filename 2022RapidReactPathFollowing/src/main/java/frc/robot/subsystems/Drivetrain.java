bd734c50d1c9748a2c96833bee35e03690b5a1bf
Vincent Wren <wrenvin@gmail.com>
2022-06-28 16:03:12 -0500

    Update Drivetrain.java

--- 2022RapidReactPathFollowing/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2022RapidReactPathFollowing/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -19,9 +19,12 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
@@ -29,26 +32,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
-	private CANSparkMax leftMotor1 = driveMotor(1, true);
-	private CANSparkMax leftMotor2 = driveMotor(2, true);
-	private CANSparkMax leftMotor3 = driveMotor(3, true);
+	private CANSparkMax leftMotor1 = driveMotor(1, false);
+	private CANSparkMax leftMotor2 = driveMotor(2, false);
+	private CANSparkMax leftMotor3 = driveMotor(3, false);
 	private CANSparkMax rightMotor1 = driveMotor(4, false);
 	private CANSparkMax rightMotor2 = driveMotor(5, false);
 	private CANSparkMax rightMotor3 = driveMotor(6, false);
-
-	private Encoder m_leftEncoder = new Encoder(0, 1, true);
-	private Encoder m_rightEncoder = new Encoder(2, 3, false);
-
-	private MotorControllerGroup leftMotors = new MotorControllerGroup(leftMotor1, leftMotor2, leftMotor3);
-	private MotorControllerGroup rightMotors = new MotorControllerGroup(rightMotor1, rightMotor2, rightMotor3);
-
-	private DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
-  private final Gyro m_gyro = new ADXRS450_Gyro();
-  private final DifferentialDriveOdometry m_odometry;
   
-  public Drivetrain() {
-    m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
-  }
+	private Encoder m_leftEncoder = new Encoder(0, 1, false, EncodingType.k4X);
+	private Encoder m_rightEncoder = new Encoder(2, 3, false, EncodingType.k4X);
+
+  private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+  private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
+
+	private MotorControllerGroup m_leftMotors = new MotorControllerGroup(leftMotor1, leftMotor2, leftMotor3);
+	private MotorControllerGroup m_rightMotors = new MotorControllerGroup(rightMotor1, rightMotor2, rightMotor3);
+
+	private DifferentialDrive drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+  private final Gyro m_gyro = new ADXRS450_Gyro();
+
+  // Odometry class for tracking robot pose
+  private final DifferentialDriveOdometry m_odometry;
 
   public CANSparkMax driveMotor(int motorID, boolean inverted) {
 		CANSparkMax sparkMax = new CANSparkMax(motorID, MotorType.kBrushless);
@@ -56,51 +60,147 @@ public class Drivetrain extends SubsystemBase {
 		sparkMax.setInverted(inverted);
 		sparkMax.setIdleMode(IdleMode.kBrake);
 		sparkMax.setSmartCurrentLimit(40);
-    return sparkMax;
+		
+		return sparkMax;
   }
 
-  public void driveWithJoysticks(double xSpeed, double zRotation) {
-		drive.arcadeDrive(xSpeed, zRotation, true);
-	}
+  /** Creates a new DriveSubsystem. */
+  public Drivetrain() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    //m_rightMotors.setInverted(true);
+    //int encoding_scale = 
 
-  public void arcadeDrive(double xSpeed, double zRotation) {
-		drive.arcadeDrive(xSpeed, zRotation, false);
-	}
+    // Sets the distance per pulse for the encoders
+    m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+    m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+
+    resetEncoders();
+    m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+  }
 
   @Override
   public void periodic() {
+    // Update the odometry in the periodic block
     m_odometry.update(
         m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
-    
-    // This method will be called once per scheduler run
-    m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
-    m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+    SmartDashboard.putNumber("ENCODING SCALE", m_leftEncoder.getEncodingScale());
+      }
+
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
   }
 
-  public void stopMotors() {
-		drive.stopMotor();
-	}
-
+  /**
+   * Returns the current wheel speeds of the robot.
+   *
+   * @return The current wheel speeds.
+   */
   public DifferentialDriveWheelSpeeds getWheelSpeeds() {
     return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
   }
 
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    m_odometry.resetPosition(pose, m_gyro.getRotation2d());
+  }
+
+  /**
+   * Drives the robot using arcade controls.
+   *
+   * @param fwd the commanded forward movement
+   * @param rot the commanded rotation
+   */
+  public void arcadeDrive(double fwd, double rot) {
+    drive.arcadeDrive(fwd, rot);
+  }
+
+  /**
+   * Controls the left and right sides of the drive directly with voltages.
+   *
+   * @param leftVolts the commanded left output
+   * @param rightVolts the commanded right output
+   */
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    m_leftMotors.setVoltage(leftVolts);
+    m_rightMotors.setVoltage(rightVolts);
+    drive.feed();
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+
+  /**
+   * Gets the average distance of the two encoders.
+   *
+   * @return the average of the two encoder readings
+   */
+  public double getAverageEncoderDistance() {
+    return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
+  }
+
+  /**
+   * Gets the left drive encoder.
+   *
+   * @return the left drive encoder
+   */
+  public Encoder getLeftEncoder() {
+    return m_leftEncoder;
+  }
+
+  /**
+   * Gets the right drive encoder.
+   *
+   * @return the right drive encoder
+   */
+  public Encoder getRightEncoder() {
+    return m_rightEncoder;
+  }
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   *
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput) {
+    drive.setMaxOutput(maxOutput);
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
   public double getHeading() {
     return m_gyro.getRotation2d().getDegrees();
   }
 
-  public Pose2d getPose() {
-    return m_odometry.getPoseMeters();
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return m_gyro.getRate();
   }
-  public void tankDriveVolts(double leftVolts, double rightVolts) {
-    leftMotors.setVoltage(leftVolts);
-    rightMotors.setVoltage(rightVolts);
-    drive.feed();
-  }
-
-  public void resetOdometry(Pose2d pose) {
-		m_leftEncoder.reset();
-		m_rightEncoder.reset();
-		m_odometry.resetPosition(pose, m_gyro.getRotation2d());
-	  }
 }
