1d21687d70964981623cbe51895cfe1461aa6cf8
Vincent Wren <wrenvin@gmail.com>
2022-06-28 16:02:54 -0500

    Update Constants.java

--- 2022RapidReactPathFollowing/src/main/java/frc/robot/Constants.java
+++ 2022RapidReactPathFollowing/src/main/java/frc/robot/Constants.java
@@ -9,27 +9,30 @@ import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * Constants. This class should not be used for any other purpose. All Constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * Constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public final static double ACUTAL_WHEEL_DIAMETER = Units.inchesToMeters(4.1966);
-	public final static double ENCODER_CONVERSION_FACTOR = (Math.PI * ACUTAL_WHEEL_DIAMETER) / 1024;
-    public static double ksVolts = 0.15071;
-    public static double kvVoltSecondsPerMeter = 1.5545;
-    public static double kaVoltSecondsSquaredPerMeter = 0.32436;
+	//public final static double ENCODER_CONVERSION_FACTOR = (Math.PI * ACUTAL_WHEEL_DIAMETER) / 1024;
+    private static final int kRotPerTick = 1/4096;
+    private static final double kGearRatio = 4.444;
+    public static double ksVolts = 0.1273;
+    public static double kvVoltSecondsPerMeter = 1.645;
+    public static double kaVoltSecondsSquaredPerMeter = 0.33145;
 	public static double kTrackWidthMeters = 0.69; //distance between wheels horizontal
     public static DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidthMeters);
-    public static double kMaxSpeedMetersPerSecond = 3;
-    public static double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static double kMaxSpeedMetersPerSecond = 0.5;
+    public static double kMaxAccelerationMetersPerSecondSquared = 0.5;
     public static double kRamseteB = 2;
     public static double kRamseteZeta = 0.7;
-    public static double kPDriveVel = 2.1468;
-	public static double kMetersPerRotation = ACUTAL_WHEEL_DIAMETER * Math.PI; // 0.3348
-    public static double kEncoderDistancePerPulse = kMetersPerRotation/4; 
+    public static double kPDriveVel = 0;//2.2558;
+	public static double kMetersPerRotation = ACUTAL_WHEEL_DIAMETER * Math.PI; // 0.3362
+    public static double kEncoderDistancePerPulse = kMetersPerRotation/1024;
+    public static int kDriverControllerPort = 0; 
 }
 //Meters per rotation 0.3191858136
 
